e2b92095ec125a5da0e43e1b76292957942fd656
Oleg Mikhailov <omikhailov@ace-st.com>
2018-03-09 13:22:06 +0200

    Main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSelectionComponent } from './thread-selection/thread-selection.component';
+import { MessageSelectionComponent } from './message-selection/message-selection.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSelectionComponent,
+    MessageSelectionComponent,
+    ThreadListComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule
